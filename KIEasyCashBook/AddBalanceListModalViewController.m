11496d1bce188671360a439f432fd2cc5d0c8063
Tre3 <keitree33@gmail.com>
2015-02-05 00:47:35 +0900

    ステータスバーの背景の調整

--- KIEasyCashBook/AddBalanceListModalViewController.m
+++ KIEasyCashBook/AddBalanceListModalViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     
     [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:0.70f green:1.00f blue:0.40f alpha:1]];
-    UIView *statusBarView =  [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 22)];
+    UIView *statusBarView =  [[UIView alloc] initWithFrame:CGRectMake(0, 0, 500, 22)];
     statusBarView.backgroundColor  =  [UIColor colorWithRed:0.70f green:1.00f blue:0.40f alpha:1];
     [self.view addSubview:statusBarView];
     
